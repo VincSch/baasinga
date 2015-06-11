4dfa2a24aa003cb93c9ba6985a16601549df63c2
VincSch <vs@stroodel.com>
2015-06-11 12:36:05 +0200

    fix pom

--- web/src/main/java/com/stroodel/baasinga/web/config/AppConfig.java
+++ web/src/main/java/com/stroodel/baasinga/web/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.stroodel.baasinga.web.config;
 
-import com.stroodel.baasinga.RestMvcConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
