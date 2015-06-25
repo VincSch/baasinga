d26d3006708d0a9da7eadd903b7dada67783113a
VincSch <vs@stroodel.com>
2015-06-25 13:27:02 +0200

    adjusted entity

---
+++ repository/src/main/java/com/vschwarzer/baasinga/repository/render/ModelDAO.java
@@ -0,0 +1,21 @@
+package com.vschwarzer.baasinga.repository.render;
+
+import com.vschwarzer.baasinga.domain.model.render.Model;
+import com.vschwarzer.baasinga.domain.model.render.Version;
+import com.vschwarzer.baasinga.repository.GenericDAO;
+
+/**
+ * Interface defining repository methods for models.
+ *
+ * @author <a href="mailto:vs@stroodel.com">Vincent Schwarzer</a>
+ */
+public interface ModelDAO extends GenericDAO<Model> {
+
+    /**
+     * Find an mdoel by its name
+     *
+     * @param name model name
+     * @return model
+     */
+    public Model findByName(String name);
+}
