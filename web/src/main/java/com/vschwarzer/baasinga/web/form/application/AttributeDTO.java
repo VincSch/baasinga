41be2444764bd4d63dd545cdc458e0111eac0a6c
VincSch <vs@stroodel.com>
2015-07-08 21:21:31 +0200

    First shot of fronted code

---
+++ web/src/main/java/com/vschwarzer/baasinga/web/form/application/AttributeDTO.java
@@ -0,0 +1,17 @@
+package com.vschwarzer.baasinga.web.form.application;
+
+/**
+ * Created by Vincent Schwarzer on 08.07.15.
+ */
+public class AttributeDTO {
+
+    private String name = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
